0bca3df7b0f7b46ca83c2bdef36cebe028c1e7da
Vasilyg10 <zvgianna@gmail.com>
2021-07-28 23:54:25 -0500

    completed routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+    addThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThoughtById,
+    removeThought,
+    addReaction, 
+    removeReaction
+  } = require('../../controllers/thought-controller');
+  
+  router.route('/').get(getAllThoughts).post(addThought);
+  router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById);
+  router.route('/:thoughtId/users/:userId').delete(removeThought);
+  router.route('/:thoughtId/reactions').post(addReaction);
+  router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
+  
+  module.exports = router;
